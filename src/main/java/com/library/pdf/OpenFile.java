19b368dc188805156dcda22b631bbb47119ce0dc
GTA <hhfhfh@gmail.com>
2022-09-13 03:32:41 +0100

    m

---
+++ src/main/java/com/library/pdf/OpenFile.java
@@ -0,0 +1,16 @@
+package com.library.pdf;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+
+public class OpenFile {
+
+    PDDocument pdDocument = new PDDocument();
+
+    public void fromUrl(){
+        System.out.println(pdDocument);
+
+    }
+
+
+
+}
