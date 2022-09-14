f609ee316ccc73bae71fe65bd3cf06a445e42e24
GTA <hhfhfh@gmail.com>
2022-09-14 22:37:00 +0100

    gmain foi alterado

--- src/main/java/com/library/pdf/Main.java
+++ src/main/java/com/library/pdf/Main.java
@@ -3,6 +3,8 @@ package com.library.pdf;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        OpenFile openFile = new OpenFile();
     }
 }
 
--- src/main/java/com/library/pdf/OpenFile.java
+++ src/main/java/com/library/pdf/OpenFile.java
@@ -4,13 +4,29 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 
 public class OpenFile {
 
-    PDDocument pdDocument = new PDDocument();
 
-    public void fromUrl(){
-        System.out.println(pdDocument);
+
+    public void fromFile() {
+
+        System.out.println("from file");
+
+
+
+
 
     }
 
 
+
+
+    private void pdfLoad(){
+
+
+        //PDDocument pd = PDDocument.load(file);
+        //PDFRenderer pr = new PDFRenderer (pd);
+        //BufferedImage bi = pr.renderImageWithDPI (0, 300);
+       // ImageIO.write (bi, "JPEG", new File (out));
+
+    }
 
 }
